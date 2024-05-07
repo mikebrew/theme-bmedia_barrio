6fec1aa43f33223d68fdb4bcfa737a683a7b1eb3
Michael Brewington <mike@brewingtonmedia.com>
2024-05-07 19:39:36 -0400

    js: btn copy

--- js/global.js
+++ js/global.js
@@ -4,13 +4,27 @@
  *
  */
 (function (Drupal) {
-
-  'use strict';
+  "use strict";
 
   Drupal.behaviors.bmedia_barrio = {
     attach: function (context, settings) {
+      var clipboard = new ClipboardJS(".btnCopy");
+      clipboard.on("success", function (e) {
+        // setTooltip("Copied!");
 
-    }
+        console.info("Action:", e.action);
+        console.info("Text:", e.text);
+        console.info("Trigger:", e.trigger);
+
+        e.clearSelection();
+      });
+
+      clipboard.on("error", function (e) {
+        console.error("Action:", e.action);
+        console.error("Trigger:", e.trigger);
+      });
+
+      $(".alertCopy", context).attr("onclick", "alert('Copied!')");
+    },
   };
-
-})(Drupal);
+})(jQuery, Drupal);
